90a410fa9f16ca0c17ac48b30e969dacdf1286a5
Gabriel Herrera <gabalva@ciencias.unam.mx>
2025-11-27 15:25:51 -0600

    Se ocultaron los puntos de control

--- public/js/teorema_intermedio_cues_dinamico.js
+++ public/js/teorema_intermedio_cues_dinamico.js
@@ -276,16 +276,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
         // Creación de puntos de control internos (puntos P y Q)
-        p5 = brd.create('point', [-3, 0], { name: 'P', trace: false, size: 4.5, color: 'grey', face: '[]', highlight: true, fixed: false });
+        p5 = brd.create('point', [-3, 0], { name: 'P', trace: false, size: 4.5, color: 'grey', face: '[]', highlight: true, fixed: false, visible: false  });
         p5.catchRadius = 30; // área táctil de 30px para capturar el toque
-        p6 = brd.create('point', [0, 0], { name: 'Q', trace: false, size: 4.5, color: 'grey', face: '[]', highlight: true, fixed: false });
+        p6 = brd.create('point', [0, 0], { name: 'Q', trace: false, size: 4.5, color: 'grey', face: '[]', highlight: true, fixed: false, visible: false  });
         p6.catchRadius = 30; // área táctil de 30px para capturar el toque
 
 
         // Creación de puntos de control internos (puntos P y Q)
-        p7 = brd.create('point', [3, 0], { name: 'R', trace: false, size: 4.5, color: 'grey', face: '[]', highlight: true, fixed: false });
+        p7 = brd.create('point', [3, 0], { name: 'R', trace: false, size: 4.5, color: 'grey', face: '[]', highlight: true, fixed: false, visible: false, visible: false });
         p7.catchRadius = 30; // área táctil de 30px para capturar el toque
-        p8 = brd.create('point', [8, 0], { name: 'T', trace: false, size: 4.5, color: 'grey', face: '[]', highlight: true, fixed: false });
+        p8 = brd.create('point', [8, 0], { name: 'T', trace: false, size: 4.5, color: 'grey', face: '[]', highlight: true, fixed: false, visible: false });
         p8.catchRadius = 30; // área táctil de 30px para capturar el toque
 
 
